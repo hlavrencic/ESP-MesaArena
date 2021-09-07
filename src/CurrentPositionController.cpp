98010ae2c5443435930cd9a8474c8ed102d1c49f
hlavrencic <hernan.hnet@gmail.com>
2021-09-06 22:08:41 -0300

    no me acuerdo que era esto

--- src/CurrentPositionController.cpp
+++ src/CurrentPositionController.cpp
@@ -46,7 +46,18 @@ NextPositionController::NextPositionController(AsyncWebServer* server,
 }
 
 void NextPositionController::goTo(const String& originId){
-  _state = _motorsController->goTo(_state);
+  auto newState = _motorsController->goTo(_state);
+  _state.x = newState.x;
+  _state.y = newState.y;
+  _state.xActual = newState.xActual;
+  _state.yActual = newState.yActual;
+  _state.delay = newState.delay;
+  _state.delayTotal = newState.delayTotal;
+  _state.xVelocidad = newState.xVelocidad;
+  _state.yVelocidad = newState.yVelocidad;
+
+  Serial.println(newState.xVelocidad);
+  Serial.println(_state.xVelocidad);
 }
 
 MotorsConfigController::MotorsConfigController(AsyncWebServer* server,
