189d84e55291b631cd1230dddfa1dc2fc3cfbb33
hlavrencic <hernan.hnet@gmail.com>
2021-10-28 23:57:24 -0300

    capturo en consola SVG

--- interface/src/components/SvgDrawing.tsx
+++ interface/src/components/SvgDrawing.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { useSvgDrawing } from 'react-hooks-svgdrawing'
 
-const Drawing = () => {
+const Drawing = (props: {onDataChange?: (data: string | null) => void}) => {
   const [
     renderRef,
     action
-  ] = useSvgDrawing()
-  return <div ref={renderRef} style ={{'height': '500px'}} />
+  ] = useSvgDrawing();
+
+  return (<div ref={renderRef} style ={{'height': '500px'}} onClick={e => {
+    let data = action.getSvgXML();
+
+    props.onDataChange && props.onDataChange(data);
+  }} />);
 };
 
 export default Drawing;
--- interface/src/project/DemoInformation.tsx
+++ interface/src/project/DemoInformation.tsx
@@ -1,11 +1,16 @@
+import { Button } from '@material-ui/core';
 import React, { Component } from 'react';
 import Drawing from '../components/SvgDrawing';
 
 class DemoInformation extends Component {
-
+  
+  
   render() {
     return (
-      <Drawing />
+      <>
+      <Drawing onDataChange={data => console.log(data)}/>
+      <Button onClick={e => {}}>SEND</Button>
+      </>
     )
   }
 
